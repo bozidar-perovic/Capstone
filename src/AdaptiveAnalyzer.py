703e74885b18d74a9fd2ebf04e74ceed158332c2
Bozidar Perovic <bozidarnoob2@gmail.com>
2026-09-25 13:31:55 -0400

    First draft of how OOP can be used here. Will change it more

---
+++ src/AdaptiveAnalyzer.py
@@ -0,0 +1,6 @@
+from BaseAnalyzer import BaseAnalyzer
+
+class AdaptiveAnalyzer(BaseAnalyzer):
+
+	def __init__(self):
+		super().__init__()
---
+++ src/RuleBasedAnalyzer.py
@@ -0,0 +1,6 @@
+from BaseAnalyzer import BaseAnalyzer
+
+class RuleBasedAnalyzer(BaseAnalyzer):
+
+	def __init__(self):
+		super().__init__()
